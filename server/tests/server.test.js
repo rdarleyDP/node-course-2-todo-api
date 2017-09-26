3abd5f5d30addfba859834cefb955854aa8266ee
Robin Darley <rdarley@developerprogram.com>
2017-09-26 11:39:05 -0500

    Add tests for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'First test todo'
 }, {
+    _id: new ObjectID(),
     text: 'Second test todo'
 }];
 
@@ -66,6 +69,36 @@ describe('GET /todos', () => {
         .expect((res) => {
             expect(res.body.todos.length).toBe(2);
         })
+        .end(done);
+    });
+});
+
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(todos[0].text);
+        })
+        .end(done);
+    });
+    
+    it('should return 404 if todo not found', (done) => {
+        var testID = new ObjectID();
+        request(app)
+        .get(`/todos/${testID.toHexString()}`)
+        .expect(404)
+        .end(done);
+    });
+    
+    it('should return 404 for non-object ids', (done) => {
+        // /todos/123
+        // make sure status is 404
+        var nonObj = '123';
+        request(app)
+        .get(`/todos/${nonObj}`)
+        .expect(404)
         .end(done);
     });
 });
