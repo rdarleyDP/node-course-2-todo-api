161af5cdca89d90ef4a7a5aa8ae0e0ac7365f9a0
Robin Darley <rdarley@developerprogram.com>
2017-09-27 14:34:55 -0500

    Configure test database

--- server/server.js
+++ server/server.js
@@ -3,13 +3,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
+var config = require('./config/config');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT;
 
 app.use(bodyParser.json());
 
